d0cd25411616b8db2689fcc5241d664884d5cabe
chen <chenzhe930204@163.com>
2020-08-10 21:27:34 +0800

    git init

---
+++ src/main/java/com/chenzhe/learn/design/pattern/DesignpatternApplication.java
@@ -0,0 +1,13 @@
+package com.chenzhe.learn.design.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignpatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignpatternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chenzhe/learn/design/pattern/DesignpatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenzhe.learn.design.pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
