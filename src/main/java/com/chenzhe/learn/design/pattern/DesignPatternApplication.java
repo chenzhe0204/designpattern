cca02e5c273cbaec2b86cdae308ad585c4649f76
chen <chenzhe930204@163.com>
2020-08-12 23:51:28 +0800

    项目初始化

--- src/main/java/com/chenzhe/learn/design/pattern/DesignPatternApplication.java
+++ src/main/java/com/chenzhe/learn/design/pattern/DesignPatternApplication.java
@@ -3,11 +3,14 @@ package com.chenzhe.learn.design.pattern;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author cz
+ */
 @SpringBootApplication
-public class DesignpatternApplication {
+public class DesignPatternApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DesignpatternApplication.class, args);
+        SpringApplication.run(DesignPatternApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/chenzhe/learn/design/pattern/test/TestController.java
@@ -0,0 +1,18 @@
+package com.chenzhe.learn.design.pattern.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TestController
+ * @author cz
+ * @date 2020-08-11
+ */
+@RestController("/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello Word";
+    }
+}
